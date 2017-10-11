b97d3189822e5a0749f59dcbc17c328dccbf255a
adsons <adsons@SBES3T3120.setranspe.net>
2017-10-11 16:38:20 -0300

    Atualizações e criação da livraria

--- jsf-livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ jsf-livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -4,47 +4,26 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class LivroBean {
-	private String titulo;
-	private String isbn;
-	private double preco;
-	private String dataLancamento;
+	
+	private Livro livro = new Livro();
+	
+	
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+
+
 
 	public void gravar() {
-		System.out.println("Gravando Livro LivroBean [titulo=" + titulo + ", isbn=" + isbn + ", preco=" + preco + ", dataLancamento="
-				+ dataLancamento + "]");
+		System.out.println("Gravando Livro LivroBean [titulo=" + livro.getTitulo() + ", isbn=" + livro.getIsbn() + ", preco=" + livro.getPreco() + ", dataLancamento="
+				+ livro.getDataLancamento() + "]");
 	}
 
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getIsbn() {
-		return isbn;
-	}
-
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-
-	public double getPreco() {
-		return preco;
-	}
-
-	public void setPreco(double preco) {
-		this.preco = preco;
-	}
-
-	public String getDataLancamento() {
-		return dataLancamento;
-	}
-
-	public void setDataLancamento(String dataLancamento) {
-		this.dataLancamento = dataLancamento;
-	}
-
-	
 }
