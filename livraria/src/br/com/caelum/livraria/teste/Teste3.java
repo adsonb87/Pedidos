85aba1a56557994f598bb49b4a6310c3a758f659
Adson Barbosa de Souza <adsons@setranspe.net>
2017-10-30 12:32:46 -0300

    Atualização

--- livraria/src/br/com/caelum/livraria/teste/Teste3.java
+++ livraria/src/br/com/caelum/livraria/teste/Teste3.java
@@ -1,15 +1,9 @@
 package br.com.caelum.livraria.teste;
 
-import java.io.IOException;
-
-import br.com.caelum.livraria.modelo.ValidaPedido;
-
 public class Teste3 {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args){
 
-		ValidaPedido pedido = new ValidaPedido("PedidoTeste3.txt");
 
-		pedido.validaArquivoPedido();
 
 	}
 }
